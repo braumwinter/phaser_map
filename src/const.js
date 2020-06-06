e8ffedcffabc003094e87347add81924b00957ab
Kate <braumwinter@gmail.com>
2020-06-06 18:47:39 +0300

    feat: add const

---
+++ src/const.js
@@ -0,0 +1,39 @@
+export const STEP_MAP = 256;
+export const STEP_TREE = 64;
+
+export const EXTENSION_TGA = '.tga';
+export const EXTENSION_PNG = '.png';
+export const EXTENSION_JPG = '.jpg';
+
+export const ROAD_PATH = './../assets/roads/';
+export const ROAD_CROSS_X = 'road_cross_x';
+export const ROAD_CROSS_T_TO_UP = 'road_cross_t_to_up';
+export const ROAD_CROSS_T_TO_RIGHT = 'road_cross_t_to_right';
+export const ROAD_CROSS_T_TO_DOWN = 'road_cross_t_to_down';
+export const ROAD_CROSS_T_TO_LEFT = 'road_cross_t_to_left';
+export const ROAD_UP_TO_RIGHT = 'road_up_to_right';
+export const ROAD_UP_TO_LEFT = 'road_up_to_left';
+export const ROAD_RIGHT_TO_UP = 'road_right_to_up';
+export const ROAD_RIGHT_TO_DOWN = 'road_right_to_down';
+export const ROAD_DOWN_TO_RIGHT = 'road_down_to_right';
+export const ROAD_DOWN_TO_LEFT = 'road_down_to_left';
+export const ROAD_LEFT_TO_UP = 'road_left_to_up';
+export const ROAD_LEFT_TO_DOWN = 'road_left_to_down';
+export const ROAD_HORIZONTAL = 'road_horizontal';
+export const ROAD_VERTICAL = 'road_vertical';
+
+export const STREET_PATH = './../assets/street/';
+export const STREET_DALLE = 'street_dalle';
+export const STREET_GRASS = 'street_grass';
+
+export const BUILD_PATH = './../assets/build/';
+export const BUILD = 'build';
+export const BUILD_GAME = 'build_game';
+
+export const TREE_PATH = './../assets/tree/';
+export const TREE_1 = 'tree_1';
+export const TREE_2 = 'tree_2';
+
+export const BOX_PATH = './../assets/box/';
+export const BOX = 'box';
+export const BONUS = 'bonus';
